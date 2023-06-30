6ce6ac9f4a8265c54e26cc51f2a4d04b7dd0bae6
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-06-30 10:50:52 +0300

    New Changes

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
   /* Access all the five animals buttons and a section for displaying*/
   let section = document.getElementById("section")
-  let buttons = document.querySelectorAll(".button")
-  let body = document.getElementById("body")
+   let body = document.getElementById("body")
    
   
   /*Elements being created*/
   let title =document.createElement("h2")
   let image = document.createElement("img")
-  
-  
-  let voteNumber = document.getElementById("vote")
- 
-  
   
   
   /* Function for all the animals loading */
@@ -20,26 +14,16 @@
       if(section.style.display == "none"){
          section.style.display = "block"
          body.style.display = "none"
-         
-        
+           
          title.textContent = value.name;
-         section.append(title)
-         
+         section.append(title)  
          
          image.src = value.image;
-         section.append(image)
-         
-         
-      
-        voteNumber.textContent = "Votes:" + value.votes;
+         section.append(image) 
       } else{
          section.style.display = "none"
       }
      }
-
-
-
- 
  
  //function for going back to home page
  let homeButton = document.getElementById("home")
@@ -49,62 +33,61 @@
    section.style.display ="none"
    title.textContent=""
    image.src = ""
-   voteNumber.textContent = 'Votes:' + value.votes
  })
 
-
- 
- 
-
- 
- 
- 
- 
- 
  // Function for the vote button
+ let voteNumber = document.getElementById("vote")
+function vote(detail){
+  voteNumber.addEventListener("click" ,()=>{
+      voteNumber.textContent = "Votes:" + detail.votes;
+       detail.votes +=1;
+       voteNumber.textContent = "Votes:" + detail.votes
 
- function vote(detail){
-   fetch(`http://localhost:3000/characters/${detail.id}`)
-      .then(response=> response.json())  
-      .then(data =>{
-       voteNumber.addEventListener("click" ,()=>{
-          data.votes += 1
-          voteNumber.textContent = "Votes:"+ data.votes
-
-          reset(data)//Function for reset
-      }
-   )
-   
+      
+       fetch(`http://localhost:3000/characters/${detail.id}`,{
+         method: "PATCH",
+         headers:{
+            'Content-Type': 'application/json',
+         },
+         body:JSON.stringify({votes:detail.votes})
+      })    
    })
 }
  
 
-
-
-
 // This function now fetches the data and displays it an even votting
-
+var buttons = document.querySelectorAll(".button")
 buttons.forEach(button =>{
    button.addEventListener('click',()=>{
        fetch(`http://localhost:3000/characters/${button.id}`)
       .then(response=> response.json())  
       .then((data)=>{
+         console.log(data)
           first(data,section)// Function for display
           vote(data) //Function for voting
-          
+          reset(data)
       })
    })
  })
 
 
+ 
  //For the Reset Button
- let resetButton = document.getElementById("reset")
  
  function reset(value){
-   resetButton.addEventListener("click", ()=>{
-      value.votes = 0
-      voteNumber.textContent= "Votes:" + value.votes
-   })
- }
+   let resetButton = document.getElementById("reset")  
 
+   resetButton.addEventListener("click" ,()=>{
+      value.votes *=0;
+      voteNumber.textContent = "Votes:" + value.votes
  
+     
+      fetch(`http://localhost:3000/characters/${value.id}`,{
+        method: "PATCH",
+        headers:{
+           'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({votes:value.votes})
+     }) }
+ )
+}
